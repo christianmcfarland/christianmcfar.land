2f1af2baed6cdc8f2b73646d2dce4e404d2b9766
Christian McFarland <christianmcfarland@gmail.com>
2024-05-23 20:09:10 -0400

    Mobile styling; Resume downloads

--- src/pages/ResumePage.vue
+++ src/pages/ResumePage.vue
@@ -3,7 +3,24 @@
     <q-page-container class="q-mt-xl">
       <h2 class="text-white">Resume</h2>
       <q-card class="q-mt-lg">
-        <q-btn icon="download" class="full-width">Download PDF</q-btn>
+        <q-btn
+          href="src/assets/resume.pdf"
+          download="Christian-McFarland-Resume-2024"
+          target="_blank"
+          icon="picture_as_pdf"
+          class="full-width"
+          >Download .pdf</q-btn
+        >
+      </q-card>
+      <q-card class="q-mt-md">
+        <q-btn
+          href="src/assets/resume.txt"
+          download="Christian-McFarland-Resume-2024"
+          target="_blank"
+          icon="description"
+          class="full-width"
+          >Download .txt</q-btn
+        >
       </q-card>
       <q-card class="q-mt-md">
         <img width="100%" src="~src/assets/resume.jpg" />
